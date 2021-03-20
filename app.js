4cab88dbbe91e095e3424427b3810299223540cd
SebastianMitco <70155015+SebastianMitco@users.noreply.github.com>
2021-03-20 13:13:26 +0100

    add refresh button and hide monkey

--- app.js
+++ app.js
@@ -1,29 +1,37 @@
 const closedEar = document.querySelector('.closed')
 const openFace = document.querySelector('.open')
 const clickText= document.querySelector('.text')
+const btn= document.querySelector('.btn')
+const reload= document.querySelector('.reload')
 tired = ()=> {
     if(clickCount <19){
         document.querySelector('.text').innerHTML= "Click the monkey 20 times!"
-        
     ;
-    } else{document.querySelector('.text').innerHTML= "The monkey is tired"
+    } else{document.querySelector('.text').innerHTML= "<h4>The monkey is tired!<h5> <br><h4>Reload the page to get another monkey.<h3>"
     console.log("changed")
-    closedEar.classList.remove('active')
-    openFace.classList.remove('active')
+        if(btn.classList.contains('btn')){
+            btn.classList.add('closed')
+        };
+        if(reload.classList.contains('reload')){
+            reload.classList.remove('closed')
+        }
     }
 }
 
 var clickCount=0;
 tired();
-document.querySelector('.clicks').innerHTML= clickCount;
+
 addClick = ()=> {
 clickCount++;
+document.querySelector('.clicks').innerHTML= clickCount;
 console.log(clickCount)
 }
 
 
 //Add event listener
-
+reload.addEventListener('click',()=>{
+    location.reload();
+})
 closedEar.addEventListener('click',()=>{
     if(openFace.classList.contains('open')){
         openFace.classList.add('active');
